e1a6d5ba5f407506ba6a3ce6f064e504d49ebc43
ideni44 <thedenike@gmail.com>
2022-12-24 13:10:23 +0300

    'edited'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello world. It is the next work! Wow, nice to see you!'
+    return 'Hello world. It is the next work! Happy to see you!'
 
 if __name__ == '__main__':
     app.run(debug=True)
